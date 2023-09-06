d2ecbeadd84d318bda0f59e03d920be7d8a98b0f
kaka <ntnghia.ngo@gmail.com>
2023-09-06 11:41:57 +0700

    add Type for Slider components

--- src/types/styleTypes.ts
+++ src/types/styleTypes.ts
@@ -1,3 +1,11 @@
 export type ArrowProps = Partial<{
   direction: string;
 }>;
+
+export type SlideProps = Partial<{
+  bg: string;
+}>;
+
+export type WrapperProps = Partial<{
+  slideIndex: number | undefined;
+}>;
