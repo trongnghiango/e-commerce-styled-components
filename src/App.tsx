36ddee598367f34170bb8ecd41e1b5a09ff0d3e7
kaka <ntnghia.ngo@gmail.com>
2023-09-06 18:01:03 +0700

    fix file App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import ProductListPage from './pages/ProductListPage';
+import ProductPage from './pages/ProductPage';
+import RegisterPage from './pages/Register';
+import LoginPage from './pages/Login';
+import CartPage from './pages/CartPage';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="product-list" element={<ProductListPage />} />
+      <Route path="product" element={<ProductPage />} />
+      <Route path="register" element={<RegisterPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="cart" element={<CartPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
